87c9a59e5d269194deb7eba4e89d2650b7c798d2
zablon <imjunior254@gmail.com>
2023-09-02 22:30:36 +0300

    adding users accounts and setting authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL='login/'
+LOGIN_REDIRECT_URL='blog:home'
+LOGOUT_REDIRECT_URL='blog:home'
