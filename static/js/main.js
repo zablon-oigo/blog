f1877c30f605045f1f85ff90e0f42ef0941c2907
zablon <imjunior254@gmail.com>
2023-09-03 15:24:22 +0300

    adding grid box to the index page

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
 let blogApp=()=>{
     let dismissButton=document.querySelector('#dismiss-alert')
-    setTimeout(blogApp,2000)  
-    console.log('hello World')
+    dismissButton.classList.toggle('hidden')
+    
 }
+setTimeout(blogApp,3000) 
+
+
+// document.addEventListener('click',()=>{
+//     let dismissButton=document.querySelector('#dismiss-alert')
+//     dismissButton.classList.toggle('hidden')
+           
+// })
