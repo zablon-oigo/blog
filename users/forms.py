63be3ccac498977a220cb4d1949dbd0635084fe7
zablon <imjunior254@gmail.com>
2023-09-04 09:16:52 +0300

    styling the login page

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
-    username=forms.CharField(max_length=65)
-    password=forms.CharField(max_length=65, widget=forms.PasswordInput)
-
+    username=forms.CharField(max_length=65, widget=forms.TextInput(attrs={
+        'placeholder':'Enter your username',
+        'class':'w-full py-2 px-4 rounded-xl mb-2 '
+    }))
+    password=forms.CharField(max_length=65, widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter your password',
+        'class':'w-full px-4 py-2 rounded-xl mb-2'
+    }))
+   
 class RegForm(UserCreationForm):
     class Meta:
         model=User
