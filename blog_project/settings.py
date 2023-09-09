d3bff53e9a47c34632c6fbded3e7aa8f1b3ee0ff
zablon <zablon51@gmail.com>
2023-09-09 15:10:03 +0300

    adding requirement.txt file

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'blog',
+		'USER': 'root',
+		'PASSWORD': 'pass123',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
