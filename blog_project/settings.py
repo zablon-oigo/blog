9bb94ab60aa9f295ea99d3657b89b761a6beebea
zablon <zablonoigo51@gmail.com>
2023-09-17 10:57:07 +0300

    changing databse

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'blog',
-		'USER': 'root',
-		'PASSWORD': 'pass123',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'blog',
+# 		'USER': 'root',
+# 		'PASSWORD': 'pass123',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
 
 
 # Password validation
