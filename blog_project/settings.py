1b446470fded9d3ba19b21a8001073dd27ea1a58
zablon <zablonoigo51@gmail.com>
2023-09-17 10:48:47 +0300

    adding apps to settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'users',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
