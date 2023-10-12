0a02b17e0e647d1a26479d86a0ae7caceff48836
zablon <zablonoigo51@gmail.com>
2023-10-12 06:30:07 +0300

    configuring white noise

--- blog/models.py
+++ blog/models.py
@@ -23,4 +23,19 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.title}'
     
-  
+class Comment(models.Model):
+    post=models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+    class Meta:
+        ordering=['created']
+        indexes=[
+            models.Index(fields=['created']),
+            ]
+
+    def __str__(self):
+        return f'comment by {self.name} on {self.post}'
+   
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -130,7 +130,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build', 'static')
